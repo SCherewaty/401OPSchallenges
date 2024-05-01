4c546f3437f43d43e430130605868610b34c9791
steve <scherewaty@gmail.com>
2024-05-01 12:28:44 -0500

    class02.py

--- class02.py
+++ class02.py
@@ -5,44 +5,50 @@
 #Purpose: Pinging Python
 
 
-#Using the library datetime, print out current date and time
-#import datetime
-
+#import ping  (struggled with this)
 import ping3
 
-# Define target IP address
-target_ip = "192.168.40.135"
+import os
+import time
 
-# Send 5 pings with 40 bytes of data
-for i in range(5):
-  response = ping3.ping(target_ip)
-  if response:
-    print("Ping to successful! " + str(response))
+def ping_ip(ip):
+  response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1") 
+  if response == 0:
+    return "Succeeded"
   else:
-    print("Ping to failed.")
+    return "Failed"
+  
+def main():
+    ip = "192.168.40.135"
+    while True:
+      status = ping_ip(ip)
+      timestamp = time.strftime("%Y-%m-%d %H:%M:%S:%f")
+      print(f"{timestamp} Network {status} to {ip}")
+      time.sleep(2)
+      
+if __name__ == "__main__":
+    main()
+    
+      
+      
+      
+      #First pass attempt
 
+# Define target IP address
+# target_ip = "192.168.40.135"
 
-#Transmit a single ICMP (ping) packet to a specific IP every two seconds.
-import ping 
-import time 
-
-#Target IP address
-target_ip = "192.168.40.135"
-
-# while True:
-#     response = ping.ping(target_ip, timeout=1)
+# # Send pings to IP every 2 seconds
+# for i in range(5):
+#   response = ping3.ping(target_ip)
+#   if response:
+#     print("Ping successful! " + str(response))
+#   else:
+#     print("Ping failed.")
+    
+    
+#     time.sleep(2)
     
 
-# #Evaluate the response as either success or failure.
-# if response.alive:
-#     print(f"Ping to {target_ip} successful. (RTT: {response.rtt}ms)")
-# else:
-#     print(f"Ping to {target_ip} failed.")
-    
-# #Assign success or failure to a status variable.
 
 
-# #For every ICMP transmission attempted, print the status variable along with a comprehensive timestamp and destination IP tested.
-
-# #from datetime import date 
 
