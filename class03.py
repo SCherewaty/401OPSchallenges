932b7f3f4a428d2ffcd6ba527dd46538b0c798d4
steve <scherewaty@gmail.com>
2024-05-01 21:49:26 -0500

    class03.py

--- class03.py
+++ class03.py
@@ -6,6 +6,32 @@
 
 import smtplib, os
 
+import ping3
+
+import os
+import time
+
+def ping_ip(ip):
+# Needed help with line 16 and used Ethan's review code
+  response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1") 
+  if response == 0:
+    return "Succeeded"
+  else:
+    return "Failed"
+  
+def main():
+    ip = "192.168.40.135"
+    while True:
+      status = ping_ip(ip)
+# Also needed help with line 27-29 and used Ethan's review code
+      timestamp = time.strftime("%Y-%m-%d %H:%M:%S:%f")
+      print(f"{timestamp} Network {status} to {ip}")
+      time.sleep(2)
+      
+if __name__ == "__main__":
+    main()
+    
+
 
 
 
