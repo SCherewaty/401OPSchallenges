c645dd85789b49975d2dcb8f639ee7404739041e
steve <scherewaty@gmail.com>
2024-06-11 07:35:56 -0500

    class31.py

--- class31.py
+++ class31.py
@@ -3,34 +3,23 @@
 #Author: Steve Cherewaty
 #Date: 06/10/2024
 #Purpose: Signature based malware PART 1
-#Sources: 
-# Worked with Brad Baack on this Ops Challenge
+#Sources: https://www.howtogeek.com/112674/how-to-find-files-and-folders-in-linux-using-the-command-line/
+# https://www.howtogeek.com/206097/how-to-use-find-from-the-windows-command-prompt/
+# 
 
-import time  # Import time module for delays
-import getpass  # Import getpass module to securely get user passwords
-import os  # Import os module for file system operations
-import paramiko  # Import paramiko for SSH connections
-import zipfile  # Import zipfile for handling zip files
-import logging  # Import logging module
-from logging.handlers import RotatingFileHandler  # Import RotatingFileHandler for log rotation
+import os
+import platform
 
-# [MOD] Configure logging with rotation, file, and stream handlers
-def setup_logger(log_file):
-    logger = logging.getLogger("BruteForceLog")
-    logger.setLevel(logging.DEBUG)
-    
-    # [MOD] Add a rotating handler
-    rotating_handler = RotatingFileHandler(log_file, maxBytes=2000, backupCount=5)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    rotating_handler.setFormatter(formatter)
-    
-    # [MOD] Add a file handler
-    file_handler = logging.FileHandler(log_file)
-    file_handler.setFormatter(formatter)
-    
-    # [MOD] Add a stream handler
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
+def search_files(filename, search_directory):
+    file_count = 0
+    hit_count = 0
+
+    for root, dirs, files in os.walk(search_directory):
+        file_count += len(files)
+        for name in files:
+            if name == filename:
+                hit_count += 1
+                print(f"File found: {os.path.join(root, name)}")
     
 
 
