957e81699d4262a6d7e6cbb83b954316816e53ad
steve <scherewaty@gmail.com>
2024-06-18 20:31:24 -0500

    class37.py

---
+++ class37.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# The below Python script shows one possible method to return the cookie from a site that supports cookies.
+
+import requests
+
+# targetsite = input("Enter target site:") # Uncomment this to accept user input target site
+targetsite = "http://www.whatarecookies.com/cookietest.asp" # Comment this out if you're using the line above
+response = requests.get(targetsite)
+cookie = response.cookies
+
+def bringforthcookiemonster(): # Because why not!
+    print('''
+
+              .---. .---.
+             :     : o   :    me want cookie!
+         _..-:   o :     :-.._    /
+     .-''  '  `---' `---' "   ``-.
+   .'   "   '  "  .    "  . '  "  `.
+  :   '.---.,,.,...,.,.,.,..---.  ' ;
+  `. " `.                     .' " .'
+   `.  '`.                   .' ' .'
+    `.    `-._           _.-' "  .'  .----.
+      `. "    '"--...--"'  . ' .'  .'  o   `.
+
+        ''')
+
+bringforthcookiemonster()
+print("Target site is " + targetsite)
+print(cookie)
