97c6457c95359120b91a483c2d71e835e3e338fd
steve <scherewaty@gmail.com>
2024-06-24 19:41:39 -0500

    class41.py

---
+++ class41.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#Author: Steve Cherewaty
+#Date: 06/24/2024
+#Purpose: Attack Tools Pt 1
+#Sources: 
+# 
+# https://thepythoncode.com/article/make-a-xss-vulnerability-scanner-in-python
+# Conferred with ChatGPT for this assignment
